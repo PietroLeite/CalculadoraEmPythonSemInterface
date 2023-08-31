2dba0494dc25436e9efd1ef751b30cc96241ffc0
Pietro Leite <86385443+PietroLeite@users.noreply.github.com>
2023-08-31 13:49:46 -0300

    Mudança para Match Case
    Substituindo os ifs para match case para um codigo mais limpo

--- opcoes.py
+++ opcoes.py
@@ -9,22 +9,21 @@ def menul():
     print("Sair: 0 | Soma: 1| Subtração: 2| Divisão: 3| Multiplicação: 4 | 5: Escolher novos numeros")
     while True:
         opcao = int(input("Informe a operaçao que deseja fazer: "))
-        if opcao == 1:
-            calculos.sum(x, y)
-        elif opcao == 2:
-            calculos.sub(x, y)
-        elif opcao == 3:
-            calculos.division(x, y)
-        elif opcao == 4:
-            calculos.multi(x, y)
-        elif opcao == 0:
-            print("Saindo!")
-            break
-        elif opcao == 5:
-            x = 0
-            y = 0
-            x = int(input("Informe um Numero: "))
-            y = int(input("Informe um Numero: "))
-            print(f'Novos numeros escolhidos foram: {x} e {y}')
-        else:
-            print("opcao invalida")
+        match opcao:
+            case 0:
+                print("Saindo!")
+                break
+            case 1:
+                calculos.sum(x, y)
+            case 2:
+                calculos.sub(x, y)
+            case 3:
+                calculos.division(x, y)
+            case 4:
+                calculos.multi(x, y)
+            case 5:
+                x = int(input("Informe um Numero: "))
+                y = int(input("Informe um Numero: "))
+                print(f'Novos numeros escolhidos foram: {x} e {y}')
+            case _:
+                print("opcao invalida")
